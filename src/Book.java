e42d34bf47f7b50f917c7615da86d49dfbda0aa8
alexdenzero <alexdenzero@yandex.ru>
2023-03-31 03:26:01 +0300

    mO

---
+++ src/Book.java
@@ -0,0 +1,43 @@
+public class Book {
+    private  String name;
+    private Autor autor;
+    private int age;
+
+    public Book(String name, Autor autor, int age) {
+        this.name = name;
+        this.autor = autor;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+    @Override
+    public boolean equals(Object other) {
+        if (this.getClass() != other.getClass()) {
+            return false;
+        }
+        Book c2 = (Book) other;
+        return name.equals(c2.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return java.util.Objects.hash(name);
+    }
+
+    @Override
+    public String toString() {
+        return name + ": " + name;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Autor nameAutor = new Autor ("Лев Толстой");
+        Book book = new Book("Война и мир",nameAutor,1865);
+        System.out.println("Book.name = " + book.getName());
+        System.out.println("Book.nameAutor = " + nameAutor.getAutor());
+        System.out.println("Book.age = " + book.getAge());
+        book.setAge(1869);
+        System.out.println("Book.age = " + book.getAge());
+
+        System.out.println();
+
+        Autor nameAutor1 = new Autor ("Толстой Лев");
+        Book book1 = new Book("Мир и война",nameAutor, 1866);
+        System.out.println("Book.name = " + book1.getName());
+        System.out.println("Book.nameAutor = " + nameAutor1.getAutor());
+        System.out.println("Book.age = " + book1.getAge());
+        book1.setAge(1868);
+        System.out.println("Book.age = " + book1.getAge());
     }
 }
